14731185f4b738d6217fb708b4784674d0cce956
Phil_H <halloran.phil@gmail.com>
2019-02-02 19:43:13 +0000

    \n Tidy up

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 int main(int argc, char**argv)
 {
 
-   const char* fileName ="8k.wav";
+   const char* fileName = "8k.wav";
    std::vector<int> wavVect;
    FILE *fp = fopen(fileName, "rb");
 
@@ -13,16 +13,16 @@ int main(int argc, char**argv)
    wavVect = readFileData(fp, fileName);
    long long sampleCount;
 
-   for(int i = 10;i<wavVect.size(); i++)
+   for(int i = 10;i < wavVect.size(); i++)
    {
-      std::cout<<"\n"<<wavVect.at(i);
+      std::cout<<"\n" << wavVect.at(i);
       sampleCount = i;
    }
 
    fclose(fp);
-   std::cout<<"\n\n Audio file (.wav) contains "<<sampleCount<<" audio samples.\n\n";
+   std::cout << "\n\n Audio file (.wav) contains " << sampleCount << " audio samples.\n\n";
 
-   std::cout<<"\n\nDEBUG\n\n";
+   std::cout << "\n\nDEBUG\n\n";
 
 return 0;
 
