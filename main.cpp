46cbbbc7c0ae718dd62791a10e7af2a377d2c4fc
Phil_H <halloran.phil@gmail.com>
2019-02-02 19:08:09 +0000

    Simple function test call

--- main.cpp
+++ main.cpp
@@ -4,6 +4,21 @@
 int main(int argc, char**argv)
 {
 
+   const char* fileName ="8k.wav";
+   std::vector<int> WavVect;
+   FILE *fp = fopen(fileName, "rb");
+
+   std::cout<<"\n File open...!!!";
+
+   WavVect = readFileData(fp, fileName);
+
+
+
+
+
+
+   fclose(fp);
+
 
 
    std::cout<<"\n\nDEBUG\n\n";
